b89f0890f62ce327f0ce79b5e48507873623a028
HustAhq <134574955@qq.com>
2017-06-17 10:45:28 +0800

    last

---
+++ index.js
@@ -0,0 +1,104 @@
+
+
+var setLocal = {
+    save(key, value){
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+    get(key){
+       return JSON.parse(localStorage.getItem(key));
+    }
+}
+
+var list = setLocal.get('todos') || [];
+
+
+var App = new Vue({
+    el : '.main',
+    data :{
+        list: list,
+        todo: '',
+        editingTodo : '',
+        beforeTodo : '',
+        visibility : 'all'
+    },
+    watch : {
+       list : {
+           handler : function () {
+             setLocal.save('todos', this.list);
+           },
+           deep : true
+       }
+    },
+    //计算属性有缓存
+    computed: {
+        getChecked : function () {
+            return list.filter(function (todo){
+                return !todo.checked;
+            }).length;
+        },
+        filterList : function () {
+            var filter = {
+                all : function (list) {
+                    return list;
+                },
+                unfinish : function (list){
+                    return list.filter(function (item){
+                        return !item.checked;
+                    });
+                },
+                finish : function (list){
+                    return list.filter(function (item){
+                        return item.checked;
+                    });   
+                }
+            }
+            return filter[this.visibility] ? filter[this.visibility](this.list) 
+                 : this.list;
+        }
+    },
+    methods : {
+        addTodo : function () {
+            var value = this.todo;
+            if(value){
+                this.list.push({
+                    title : value,
+                    checked: false
+                });
+            }
+            this.todo = '';
+        },
+        deleteTodo : function (todo) {
+            var index = this.list.indexOf(todo);
+            this.list.splice(index,1);
+        },
+        editTodo : function (todo) {
+            this.editingTodo = todo;
+            this.beforeTodo = todo.title;
+        },
+        editedTodo : function () {
+            this.editingTodo = '';
+        },
+        cancelTodo : function (todo) {
+              todo.title = this.beforeTodo;
+              this.beforeTodo = '';
+              this.editingTodo = '';
+        }
+    },
+    directives : {
+       focus : {
+           update (el, binding) {
+               if(binding.value){
+                   el.focus();
+               }
+           }
+       }
+    }
+});
+
+//通过hash的值去改变这些all，finish，unfinish的状态。
+function hashchange () {
+    var hash = location.hash.substring(1);
+    App.visibility = hash;
+}
+hashchange ();
+window.addEventListener('hashchange', hashchange);
